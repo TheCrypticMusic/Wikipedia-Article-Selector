f26a0cbbf8033aa72b1f1d74f310b5f067e9d35b
Daniel Gibson <gibsonlp@live.co.uk>
2020-08-12 23:07:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import re 
 
 
 class ArticleSelector:
     
     url = f'https://en.wikipedia.org/wiki/'
-
+    infomation = {}
 
     def __init__(self, search_term):
         self.search_term = search_term
@@ -20,13 +21,50 @@ class ArticleSelector:
 
     def web_scraper(self):
         self.soup = BeautifulSoup(self.res.text, 'lxml')
-        self.title = self.soup.find('h1', {'class': 'firstHeading'}).get_text()
+        
+        self.navigation = self.soup.find('div', {'role': 'navigation'}).get_text().split('\n')
+        self.r = re.compile(r"([0-9]+)")
+        
+        self.page_links = list(filter(self.r.match, self.navigation))
+
+        # Puts the contents into the a dict - need to pack values into dict next
+        for i in self.page_links:
+            self.infomation.setdefault(i)
         
         
+        # Separate sections of the page 
+
+        self.article = self.soup.find('div', {'class': 'mw-parser-output'})
+        for i in self.article:
+            self.sub_cats = self.soup.find('span', {'id': 'Common_elements'})
+            if i.name == 'p':
+                print(i.text)
+            if i.name == 'h2':
+                break
+            
+        # for child in self.section.next_sibling:
+        #     print(child)
+
+
+        # for elem in self.section.find_all_next('p'):
+        #     print(elem)
+       
+        
+
+
+
+        #self.titles = self.soup.find('p')
+        # for elem in self.titles.next_siblings:
+        #     if elem.name != 'h2':
+        #         continue
+        #     print(elem)
+
+
+        # TODO: PUT CONTENT INTO A DICT
+        # T0DO: CREATE GUI
        
 
 
 user_article = ArticleSelector('football')
 user_article.test_connection()
 user_article.web_scraper()
-user_article
