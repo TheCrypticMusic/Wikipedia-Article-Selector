a77eca9860b90efb9614d6665cf77fe6d471d6f2
Daniel Gibson <gibsonlp@live.co.uk>
2020-08-12 23:07:29 +0100

    Progress

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+
+
+class ArticleSelector:
+    
+    url = f'https://en.wikipedia.org/wiki/'
+
+
+    def __init__(self, search_term):
+        self.search_term = search_term
+        
+        
+    def test_connection(self):
+        self.res = requests.get(f'{self.url}{self.search_term}')
+        if self.res.status_code == 200:
+            print('Good Link')
+        else:
+            print('Bad Link')
+
+    def web_scraper(self):
+        self.soup = BeautifulSoup(self.res.text, 'lxml')
+        self.title = self.soup.find('h1', {'class': 'firstHeading'}).get_text()
+        
+        
+       
+
+
+user_article = ArticleSelector('football')
+user_article.test_connection()
+user_article.web_scraper()
+user_article
