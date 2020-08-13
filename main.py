73962fe09165f5d704576e56812a3c5ef97c8b3b
Daniel Gibson <gibsonlp@live.co.uk>
2020-08-13 22:21:39 +0100

    TODO: Tidy up code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import search
 import requests
 from bs4 import BeautifulSoup
 import re 
@@ -5,12 +6,13 @@ import re
 
 class ArticleSelector:
     
+
     url = 'https://en.wikipedia.org/wiki/'
     infomation = {'0': 'Intro'}
-    
-   
-    
-   
+    search_not_found = 'https://en.wikipedia.org/w/index.php?sort=relevance&search='
+    search_names = {}
+
+
     def __init__(self, search_term):
         self.search_term = search_term
         
@@ -18,49 +20,59 @@ class ArticleSelector:
     def test_connection(self):
         self.res = requests.get(f'{self.url}{self.search_term}')
         if self.res.status_code == 200:
-            print('Good Link')
+            print('Article found')
         else:
-            print('Bad Link')
+            print('Article not found. Did you mean:')
+            self.res = requests.get(f'{self.search_not_found}{self.search_term}')
+            self.search_soup = BeautifulSoup(self.res.text, 'lxml')
+            self.search_results = self.search_soup.find('ul', {'class': 'mw-search-results'}).find_all_next('div', {'class': 'mw-search-result-heading'})
+            # Enumerate to add numbers to list
+            
+            for number, search in enumerate(self.search_results):
+                search = search.get_text()
+                self.search_names.setdefault(number, search)
+            
+            for key, values in self.search_names.items():
+                print(key, values)
 
+            self.choose_article = int(input())
+            # Need to remove search headings with brackets
+            self.new_search = self.search_names.get(self.choose_article)
+            self.res = requests.get(f'{self.url}{self.new_search}')
+            
     def web_scraper(self):
         self.soup = BeautifulSoup(self.res.text, 'lxml')
-        
         self.navigation = self.soup.find('div', {'role': 'navigation'}).get_text().split('\n')
+        # Regular expression used to find every section of the page in the navigation box.
         self.r = re.compile(r"([0-9]+)")
         
         self.page_links = list(filter(self.r.match, self.navigation))
-        
-        # Puts the contents into the a dict - need to pack values into dict next
+        # Puts the contents into the a dict - key = num, value = section name
+        # The number will be used to call the section and display it to the user
         for i in self.page_links:
             self.index = i.split(' ', 1)
             self.infomation.setdefault(self.index[0], self.index[1])
         
-        print(self.infomation)
-        # Separate sections of the page 
-        for i, j in self.infomation.items():
-            print(i, j)
-        
-        # TEST: CHANGE TO INPUT ONCE COMPLETED
+        # Displays to the user all the sections on that particular Wikipedia page
+        for number, section_name in self.infomation.items():
+            print(number, section_name)
         
         self.user_input = input('Enter a number: ')
-        self.f = self.infomation.get(self.user_input).split(" ")
-        
-        self.id_search = "_".join(self.f)
-        
-        
-        self.sub_cats = self.soup.find('div', {'class': 'mw-parser-output'}).find('span', {'id': f'{self.id_search}'})
-       
-        
-        for i in self.sub_cats.find_all_next(['p', 'h2', 'h3', 'h4']):
+        self.page_section = self.infomation.get(self.user_input).split(" ")
+        # Wikipedia uses the section name as ID tag with underscores for spaces EG: Toxicity of seeds = Toxicity_of_seeds
+        self.id_search = "_".join(self.page_section)
+        self.sub_category = self.soup.find('div', {'class': 'mw-parser-output'}).find('span', {'id': f'{self.id_search}'})
+        for i in self.sub_category.find_all_next(['p', 'h2', 'h3', 'h4', 'li']):
             if i.name == 'p' or i.name == 'h3' or i.name == 'h4' or i.name == 'li':
-                 print(i.text)
+                 print(f'\n{i.text}')
             else:
                  break
         
+      
     
 
 
 
-user_article = ArticleSelector('apple')
+user_article = ArticleSelector('Apple Oranges')
 user_article.test_connection()
 user_article.web_scraper()
