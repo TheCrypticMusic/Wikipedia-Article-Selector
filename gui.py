4a5c945ce03313db54547ac65435150ecdc1dc73
TheCrypticMusic <44878496+TheCrypticMusic@users.noreply.github.com>
2020-08-16 14:54:48 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,32 +1,51 @@
 import tkinter as tk
 
-
 class App(tk.Frame):
-
+    
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.widgets()
         self.pack()
-
-
+        
+        
     def widgets(self):
         """Creates the widgets for the GUI"""
         self.title = tk.Label(self, text='Wikipedia Article Selector', font='Aerial 22 bold')
         self.title.pack()
 
         self.description = tk.Label(self, text='Please enter a topic to search')
-        self.description.pack()
+        self.description.pack(pady=10)
 
         self.search_bar = tk.Entry(self)
-        self.search_bar.bind('<Return>') # ADD FUNCTION
-        self.search_bar.pack(padx=20, pady=15)
+        self.search_bar.bind('<Return>', self.content)
+        self.search_bar.pack()
+        
+        # ADD FUNCTION
+        self.search_bar.pack(ipadx=70, pady=10)
 
 
-        self.display_text = tk.Text(self, width=40, height=30)
-        self.display_text.pack()
+        self.display_navigation_results = tk.Text(self, width=20, height=30)
+        
+        self.display_navigation_results.pack(side=tk.LEFT, padx=10, pady=10)    
+        
+        self.display_content_results = tk.Text(self, width=50, height=30)
+        
+        self.display_content_results.pack(side=tk.LEFT, padx=10, pady=10)
+        
+        
+
+    def user_search(self, event=None):
+        self.search = self.search_bar.get()
+        self.search_bar.delete(0, 'end')
+        
+    
+    def content(self, event=None):
+        self.display_content_results.insert('1.0', 'TEST TEST TEST')
+        self.display_navigation_results.insert('1.0', '1.\n1.\n1.\n1.\n1.\n1.\n1.\n1.\n1.\n1.\n')
+
 
 root = tk.Tk()
-root.geometry('300x300')
+
 app = App(master=root)
 root.mainloop()
